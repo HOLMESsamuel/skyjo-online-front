a5b180fac5f5cd2585d66cd92832320182ace209
Samuel Holmes <sam.holmes@live.fr>
2022-12-29 16:29:43 +0100

    add input validation on startpage

--- src/app/startpage/startpage.component.ts
+++ src/app/startpage/startpage.component.ts
@@ -7,4 +7,49 @@ import { Component } from '@angular/core';
 })
 export class StartpageComponent {
 
+  errorNewGame : boolean = false;
+  errorNewGameMessage : string = "";
+
+  errorJoinGame : boolean = false;
+  errorJoinGameMessage : string = "";
+
+  launchGame(value : string) {
+    if(value == null || value == "") {
+      this.setErrorNewGameMessage("You need to enter a name to create a game")
+    } else {
+      this.disableErrorNewGameMessage();
+      console.log(value);
+    }
+  }
+
+  joinGame(id : string, name : string) {
+    if(id == null || id == "" || name == null || name == "") {
+      this.setErrorJoinGameMessage("You need to enter your name and the game ID to join it")
+    } else {
+      this.disableErrorJoinGameMessage();
+      console.log(name, id);
+    }
+  }
+
+  setErrorNewGameMessage(message : string) {
+    this.errorNewGame = true;
+    this.errorNewGameMessage = message;
+  }
+
+  disableErrorNewGameMessage() {
+    this.errorNewGame = false;
+    this.errorNewGameMessage = "";
+  }
+
+  setErrorJoinGameMessage(message : string) {
+    this.errorJoinGame = true;
+    this.errorJoinGameMessage = message;
+  }
+
+  disableErrorJoinGameMessage() {
+    this.errorJoinGame = false;
+    this.errorJoinGameMessage = "";
+  }
 }
+
+
