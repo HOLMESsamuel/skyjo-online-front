f7e58a9e784a50e419930ceecde9f333d55c14b6
Samuel Holmes <samuel.holmes-ext@pole-emploi.fr>
2024-02-16 15:31:54 +0100

    use https in prod

--- src/app/environments/environment.prod.ts
+++ src/app/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 // location hostname will return the host ip adress
 export const environment = {
     production: true,
-    baseUrl: "http://" + location.hostname + "/api/games/",
-    websocketUrl: 'ws://' + location.hostname + '/websocket/games/'
+    baseUrl: "https://" + location.hostname + "/api/games/",
+    websocketUrl: 'wss://' + location.hostname + '/websocket/games/'
 };
