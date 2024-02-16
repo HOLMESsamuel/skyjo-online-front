59e8d6fa60b5ed74b855387cec0a1ea3ce54785e
Samuel Holmes <samuel.holmes-ext@pole-emploi.fr>
2024-02-16 09:07:38 +0100

    configure a reverse proxy in prod

--- src/app/environments/environment.prod.ts
+++ src/app/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 // location hostname will return the host ip adress
 export const environment = {
     production: true,
-    baseUrl: "http://" + location.hostname + ":8080/games/",
-    websocketUrl: 'ws://' + location.hostname + ':8080/websocket/games/'
+    baseUrl: "http://" + location.hostname + "/api/games/",
+    websocketUrl: 'ws://' + location.hostname + '/websocket/games/'
 };
